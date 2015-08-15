5f11968ffadc65a8d6d51ad20fedea48a70e6024
Jose <sanjuanjojo@gmail.com>
2015-08-16 04:57:07 +0800

    Add Seeds

--- database/seeds/ServiceTransactionTableSeeder.php
+++ database/seeds/ServiceTransactionTableSeeder.php
@@ -31,6 +31,33 @@ class ServiceTransactionTableSeeder extends Seeder
             'user_rate' => '4',
             'employee_rate' => '4'
         ]);
+
+        ServiceTransaction:: create([
+            'field_id' => '6',
+            'user_id' => '1',
+            'employee_id' => '2',
+            'service_desc' => "I have a broken window",
+            'user_rate' => '4',
+            'employee_rate' => '4'
+        ]);
+
+        ServiceTransaction:: create([
+            'field_id' => '3',
+            'user_id' => '1',
+            'employee_id' => '3',
+            'service_desc' => "My sink broke",
+            'user_rate' => '4',
+            'employee_rate' => '4'
+        ]);
+
+        ServiceTransaction:: create([
+            'field_id' => '6',
+            'user_id' => '1',
+            'employee_id' => '2',
+            'service_desc' => "floors are broken",
+            'user_rate' => '4',
+            'employee_rate' => '4'
+        ]);
     }
 }
 
